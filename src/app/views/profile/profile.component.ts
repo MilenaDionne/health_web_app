6f17ffc2bad7f7b466da6dfc5f0487da2ae60418
MilenaDionne <33129723+MilenaDionne@users.noreply.github.com>
2020-12-09 17:26:21 -0500

    update profile and home

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {User} from '../../shared/models/user.model';
+import {MedicalStaff, User} from '../../shared/models/user.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from 'src/app/core/services/auth.service';
 import {Observable} from 'rxjs';
@@ -10,29 +10,18 @@ import {Observable} from 'rxjs';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  myArray: any[] = [];
   user: User;
-  private firstname: string;
+  @Input() users: Observable<MedicalStaff>;
 
-  constructor(private authService: AuthService, public afs: AngularFirestore) {}
+  constructor(private authService: AuthService, public afs: AngularFirestore) {
+    this.users = authService.getUser;
+    this.users.subscribe(res => {
+      console.log(res);
+    });
+  }
   firestoreDoctorsCollection = this.afs.collection('doctors');
 
-  email = this.authService.getInfo();
-  test = this.authService.getAuthState.subscribe(res => this.user = res);
-  test2 = this.authService.getAuthState.subscribe(res => this.user.firstName = res.firstName);
-
-
   ngOnInit(): void {
-    this.afs.collection('doctors').get().subscribe((ss) => {
-      ss.docs.forEach((doc) => {this.myArray.push(doc.data()); });
-    });
-    this.afs.collection('nurses').get().subscribe((ss) => {
-      ss.docs.forEach((doc) => {this.myArray.push(doc.data()); });
-    });
-    this.afs.collection('chargenurses').get().subscribe((ss) => {
-      ss.docs.forEach((doc) => {this.myArray.push(doc.data()); });
-    });
-
   }
 
 
